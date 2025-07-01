75ba762a0263a3d79bd06da8a12e7df3afb6d2b3
SG1181200783 <surendrenganeshan22@gmail.com>
2025-07-01 00:34:02 -0700

    Include DB Config

---
+++ PaymentPage.php
@@ -0,0 +1,10 @@
+<?php
+// Include DB config
+define('DB_SERVER', 'crud-db.cr4kozvtstto.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin1234!');
+define('DB_DATABASE', 'sample');
+
+
+?>
+
